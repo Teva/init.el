87631bdc0c635255756bf538d88808e3289f36fc
Teva <teva.delar@gmail.com>
2020-10-16 18:15:22 +0200

    Update init.el

--- init.el
+++ init.el
@@ -4,3 +4,6 @@
 (setq python-shell-interpreter "python3")
 (setq py-python-command "python3")
 (setq flycheck-python-pycompile-executable "python3")
+
+;; do not forget to M-x 'refresh-package'
+;; do not forget to M-x 'package install' -> 'flycheck', 'pylint', 'elpy', 'pytest'
