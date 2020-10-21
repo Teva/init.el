a699f406860d7ee3618152666f66f60935b6ad8f
Teva <teva.delar@gmail.com>
2020-10-21 10:06:22 +0200

    Update init.el
    readlines error message fix

--- init.el
+++ init.el
@@ -7,3 +7,17 @@
 
 ;; do not forget to M-x 'refresh-package'
 ;; do not forget to M-x 'package install' -> 'flycheck', 'pylint', 'elpy', 'pytest'
+
+;; the following is only needed if there is an error message including readlines
+
+(with-eval-after-load 'python
+  (defun python-shell-completion-native-try ()
+    "Return non-nil if can trigger native completion."
+    (let ((python-shell-completion-native-enable t)
+          (python-shell-completion-native-output-timeout
+           python-shell-completion-native-try-output-timeout))
+      (python-shell-completion-native-get-completions
+       (get-buffer-process (current-buffer))
+       nil "_"))))
+
+
